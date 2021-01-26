09fb32f76963c8a01cc02945e49c916356e07cce
judeadams21 <815386@mystma.org>
2021-01-26 19:48:19 +0000

    Calculator Ver. I

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(w,x, y):
+    if w == "add":
+        return x+y
+    elif w == "subtract":
+        return x-y
+    elif w == "multiply":
+        return x*y
+    else:
+        return x/y
